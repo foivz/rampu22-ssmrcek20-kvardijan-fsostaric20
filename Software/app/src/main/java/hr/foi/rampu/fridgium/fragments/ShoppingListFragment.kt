32efc0965c210fc000a13a9298a332ee3990b4d6
Stanko Smrček <ssmrcek20@foi.hr>
2022-11-06 12:06:21 +0100

    Dodan recycleView i dizajn objekta koji ce se prikazivati

--- Software/app/src/main/java/hr/foi/rampu/fridgium/fragments/ShoppingListFragment.kt
+++ Software/app/src/main/java/hr/foi/rampu/fridgium/fragments/ShoppingListFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.fridgium.R
 
 class ShoppingListFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_shopping_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById(R.id.rv_shopping_list)
     }
 }
