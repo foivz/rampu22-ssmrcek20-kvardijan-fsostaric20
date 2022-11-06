164eb4da116a584c33084800cac4680a7728affd
Stanko Smrček <ssmrcek20@foi.hr>
2022-11-06 13:20:31 +0100

    Prikaz testnih podataka

--- Software/app/src/main/java/hr/foi/rampu/fridgium/fragments/ShoppingListFragment.kt
+++ Software/app/src/main/java/hr/foi/rampu/fridgium/fragments/ShoppingListFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.fridgium.R
+import hr.foi.rampu.fridgium.adapters.ShoppingListaAdapter
+import hr.foi.rampu.fridgium.helpers.MockDataLoader
 
 class ShoppingListFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
@@ -20,5 +23,7 @@ class ShoppingListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         recyclerView = view.findViewById(R.id.rv_shopping_list)
+        recyclerView.adapter = ShoppingListaAdapter(MockDataLoader.getDemoData())
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
     }
 }
---
+++ Software/app/src/main/java/hr/foi/rampu/fridgium/helpers/MockDataLoader.kt
@@ -0,0 +1,13 @@
+package hr.foi.rampu.fridgium.helpers
+
+import hr.foi.rampu.fridgium.entities.MjernaJedinica
+import hr.foi.rampu.fridgium.entities.Namirnica
+
+object MockDataLoader {
+    fun getDemoData(): List<Namirnica> =  listOf(
+        Namirnica(1, "Jaje", 3, MjernaJedinica(1, "Komad"), 5),
+        Namirnica(2, "Mlijeko", 1, MjernaJedinica(2, "l"), 5),
+        Namirnica(3, "Maslac", 300, MjernaJedinica(3, "g"), 0),
+    )
+
+}
