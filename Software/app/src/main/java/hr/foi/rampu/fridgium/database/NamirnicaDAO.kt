a33b188ccbc432848b30509e4e9d7accd6bc3d12
Filip Šoštarić <fsostaric20@foi.hr>
2022-11-06 16:28:33 +0100

    U potpunosti razvijene nove DAO klase za recept, namirnicu i mjernu jedinicu

--- Software/app/src/main/java/hr/foi/rampu/fridgium/database/NamirnicaDAO.kt
+++ Software/app/src/main/java/hr/foi/rampu/fridgium/database/NamirnicaDAO.kt
@@ -1,36 +1,48 @@
 package hr.foi.rampu.fridgium.database
 
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
+
 import hr.foi.rampu.fridgium.entities.Namirnica
-import hr.foi.rampu.fridgium.entities.NamirnicaRecepta
 import java.sql.ResultSet
 
 const val rec: String ="namirnica"
-interface NamirnicaDAO {
+class NamirnicaDAO {
 
-    fun getNamirnice(): List<Namirnica>{
+    fun getNamirnice(): MutableList<Namirnica>{
         val sql: String = "SELECT * FROM heroku_d1561a1a0615483.$rec"
         val vrijednosti : MutableList<Namirnica> = pretvoriVrijednosti(Connector.executeSearchQuery(sql))
         return vrijednosti
     }
 
-    @Insert
-    fun insertNamirnica(vararg namirnica: Namirnica): List<Long>
 
-    @Delete
-    fun deleteNamirnica(vararg namirnica: Namirnica)
+    fun insertNamirnica(vararg namirnica: Namirnica){
+        for(nam in namirnica){
+            val id = nam.id
+            val naziv = nam.naziv
+            val sql: String = "INSERT INTO heroku_d1561a1a0615483.$rec VALUES ($id,$naziv)"
+            Connector.executeQuery(sql)
+        }
+
+    }
+
+    fun deleteNamirnica(vararg namirnica: Namirnica){
+        for(nam in namirnica){
+            val id = nam.id
+            val sql: String = "DELETE FROM heroku_d1561a1a0615483.$rec WHERE id = $id"
+            Connector.executeQuery(sql)
+        }
+    }
 
     fun pretvoriVrijednosti(resultSet: ResultSet) : MutableList<Namirnica>{
         val popis : MutableList<Namirnica> = arrayListOf()
+        val MjernaJedinicaDAO : MjernaJedinicaDAO = MjernaJedinicaDAO()
         while(resultSet.next()){
             val id: Int  = resultSet.getInt("id")
             val naziv: String  = resultSet.getString("naziv")
             val kolicina_hladnjak : Int = resultSet.getInt("kolicina_hladnjak")
             val mjernaJedinica_id : Int = resultSet.getInt("mjerna_jedinica_id")
+            val MjernaJedinica = MjernaJedinicaDAO.getMjernaJedinica(mjernaJedinica_id)
             val kolicina_kupovina : Int = resultSet.getInt("kolicina_kupovina")
-            val namirnica = Namirnica(id,naziv,kolicina_hladnjak,mjernaJedinica_id,kolicina_kupovina)
+            val namirnica = Namirnica(id,naziv,kolicina_hladnjak,MjernaJedinica,kolicina_kupovina)
             popis.add(namirnica)
         }
         return popis
--- Software/app/src/main/java/hr/foi/rampu/fridgium/database/ReceptDAO.kt
+++ Software/app/src/main/java/hr/foi/rampu/fridgium/database/ReceptDAO.kt
@@ -1,28 +1,61 @@
 package hr.foi.rampu.fridgium.database
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
-import hr.foi.rampu.fridgium.entities.NamirnicaRecepta
+
 import hr.foi.rampu.fridgium.entities.Recept
+import java.sql.ResultSet
+
 const val ime: String = "recept"
 const val rec2: String = "naziv"
-@Dao
-interface ReceptDAO {
-    @Query ("SELECT * FROM heroku_d1561a1a0615483.$ime")
-    fun getRecepti(): List<Recept>{
-        val sql: String = "SELECT * FROM heroku_d1561a1a0615483.$nam WHERE $repidd = $id"
-        val vrijednosti : MutableList<NamirnicaRecepta> = pretvoriVrijednosti(Connector.executeSearchQuery(sql))
+
+class ReceptDAO {
+    fun getRecepti(): MutableList<Recept>{
+        val sql: String = "SELECT * FROM heroku_d1561a1a0615483.$ime"
+        val vrijednosti : MutableList<Recept> = pretvoriVrijednosti(Connector.executeSearchQuery(sql))
         return vrijednosti
     }
 
-    @Query ("SELECT * FROM heroku_d1561a1a0615483.$ime WHERE $rec2 LIKE '%:naziv%'")
-    fun getRecept(naziv : String): Recept
+    fun getRecept(naziv : String): Recept{
+        val sql: String = "SELECT * FROM heroku_d1561a1a0615483.$ime WHERE $rec2 LIKE %$naziv%"
+        val vrijednost = pretvoriVrijednost(Connector.executeSearchQuery(sql))
+        return vrijednost
+    }
 
-    @Insert
-    fun insertRecept(vararg recept: Recept): List<Long>
+    fun insertRecept(vararg recept: Recept){
+        for(rep in recept){
+            val id = rep.id
+            val naziv = rep.naziv
+            val opis = rep.opis
+            val sql: String = "INSERT INTO heroku_d1561a1a0615483.$ime VALUES ($id,$naziv,$opis)"
+            Connector.executeQuery(sql)
+        }
+    }
 
-    @Delete
-    fun deleteRecept(vararg recept: Recept)
+    fun deleteRecept(vararg recept: Recept){
+        for(rep in recept){
+            val id = rep.id
+            val sql: String = "DELETE FROM heroku_d1561a1a0615483.$ime WHERE id = $id"
+            Connector.executeQuery(sql)
+        }
+    }
+
+    fun pretvoriVrijednosti(resultSet: ResultSet): MutableList<Recept>{
+        val popis : MutableList<Recept> = arrayListOf()
+        while(resultSet.next()){
+            val id: Int  = resultSet.getInt("id")
+            val naziv = resultSet.getString("naziv")
+            val opis = resultSet.getString("opis")
+            val recept = Recept(id, naziv, opis)
+            popis.add(recept)
+        }
+        return popis
+    }
+
+    fun pretvoriVrijednost(resultSet: ResultSet): Recept {
+        val id: Int  = resultSet.getInt("id")
+        val naziv: String = resultSet.getString("naziv")
+        val opis = resultSet.getString("opis")
+        val recept = Recept(id, naziv, opis)
+        return recept
+
+    }
 }
