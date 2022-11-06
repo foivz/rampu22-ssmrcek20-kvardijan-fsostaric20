a2294e122cf6d27f7cc112da804c53d8b6d31e46
Filip Šoštarić <fsostaric20@foi.hr>
2022-11-06 13:30:55 +0100

    Izrađena nova klasa NamirnicaRecepta i DAO interface za novu klasu, ovo će omogućiti pretraživanje recepata po namirnici koju tražimo

---
+++ Software/app/src/main/java/hr/foi/rampu/fridgium/database/namirnica_receptaDAO.kt
@@ -0,0 +1,26 @@
+package hr.foi.rampu.fridgium.database
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import hr.foi.rampu.fridgium.entities.Namirnica
+import hr.foi.rampu.fridgium.entities.NamirnicaRecepta
+import hr.foi.rampu.fridgium.entities.Recept
+
+const val nam: String ="namirnica_recepta"
+const val repid: String ="recept_id"
+const val namid: String ="namirnica_id"
+interface namirnica_receptaDAO {
+    @Query("SELECT * FROM heroku_d1561a1a0615483.$nam WHERE $repid = :id")
+    fun getNamirnice(id: Int): List<NamirnicaRecepta>
+
+    @Query("SELECT * FROM heroku_d1561a1a0615483.$nam WHERE $namid = :id")
+    fun getRecepti(id: Int): List<NamirnicaRecepta>
+
+
+    @Insert
+    fun insertNamirnica(vararg namirnica: Namirnica): List<Long>
+
+    @Delete
+    fun deleteNamirnica(vararg namirnica: Namirnica)
+}
