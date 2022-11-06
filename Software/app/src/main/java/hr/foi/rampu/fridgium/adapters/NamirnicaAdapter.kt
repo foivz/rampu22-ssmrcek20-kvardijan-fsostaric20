1a43963682eb3d48a010850cec2c3a616b315ea3
Karlo Vardijan <kvardijan20@foi.hr>
2022-11-06 14:23:19 +0100

    Priprema za push

--- Software/app/src/main/java/hr/foi/rampu/fridgium/adapters/NamirnicaAdapter.kt
+++ Software/app/src/main/java/hr/foi/rampu/fridgium/adapters/NamirnicaAdapter.kt
@@ -7,21 +7,19 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.fridgium.R
 import hr.foi.rampu.fridgium.entities.Namirnica
-import org.w3c.dom.Text
 
 class NamirnicaAdapter(private val namirnicaList: List<Namirnica>) : RecyclerView.Adapter<NamirnicaAdapter.NamirnicaViewHolder>() {
 
     inner class NamirnicaViewHolder(view: View): RecyclerView.ViewHolder(view){
         private val imeNamirnice: TextView
+
         init {
-            imeNamirnice = view.findViewById<TextView>(R.id.tv_ime_namirnice)
+            imeNamirnice = view.findViewById(R.id.tv_ime_namirnice)
         }
 
         fun bind(namirnica: Namirnica){
             imeNamirnice.text = namirnica.naziv
-
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NamirnicaViewHolder {
