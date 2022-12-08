ece637e73aed748cc67b6e3cb2968513e9f5c5b7
Karlo Vardijan <kvardijan20@foi.hr>
2022-12-09 00:20:26 +0100

    Promjenjeni linkovi za migrirani server za pristup bazi podataka #3

--- Software/app/src/main/java/hr/foi/rampu/fridgium/rest/RestNamirnice.kt
+++ Software/app/src/main/java/hr/foi/rampu/fridgium/rest/RestNamirnice.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RestNamirnice {
-    const val BASE_URL = "https://fridgiumappapi.herokuapp.com/"
+    const val BASE_URL = "https://site--api--hm2b4zvqj8rq.code.run/"
 
     private var instance: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
--- Software/app/src/main/java/hr/foi/rampu/fridgium/rest/RestRecept.kt
+++ Software/app/src/main/java/hr/foi/rampu/fridgium/rest/RestRecept.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RestRecept {
-    private const val BASE_URL = "https://fridgiumappapi.herokuapp.com/"
+    private const val BASE_URL = "https://site--api--hm2b4zvqj8rq.code.run/"
 
     private var instance: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
