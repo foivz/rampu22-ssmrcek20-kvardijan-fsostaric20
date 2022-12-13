260d19922b4eb5d7279131df0fcb6eabdfdae54e
Karlo Vardijan <kvardijan20@foi.hr>
2022-12-13 17:05:14 +0100

    Implementirano skrivanje Floationg Action Buttona kod prikaza hladnjaka kada se nalazimo na samom dnu. #3

--- Software/app/src/main/java/hr/foi/rampu/fridgium/fragments/FridgeFragment.kt
+++ Software/app/src/main/java/hr/foi/rampu/fridgium/fragments/FridgeFragment.kt
@@ -65,6 +65,16 @@ class FridgeFragment : Fragment() {
             ucitajSadrzajHladnjaka()
             osvjezi.isRefreshing = false
         }
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1) && recyclerView.canScrollVertically(-1)) {
+                    dodajNamirnicuUFrizider.hide()
+                }
+                else dodajNamirnicuUFrizider.show()
+            }
+        })
     }
 
     private fun ucitajSadrzajHladnjaka(){
