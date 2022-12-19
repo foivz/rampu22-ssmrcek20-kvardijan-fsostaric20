8464f87b0b19320b12f08a4dd7f39b8599497ee8
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2022-12-20 00:50:37 +0100

    Popravak učitavanja liste za kupovinu Spriječavanje učitavanja fragmenta unaprijed #28

--- Software/app/src/main/java/hr/foi/rampu/fridgium/MainActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/fridgium/MainActivity.kt
@@ -48,9 +48,10 @@ class MainActivity : AppCompatActivity() {
 
         viewPager2.adapter = mainPagerAdapter
 
-        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
-            tab.setText(mainPagerAdapter.fragmentItems[position].titleRes)
-            tab.setIcon(mainPagerAdapter.fragmentItems[position].iconRes)
-        }.attach()
+        TabLayoutMediator(tabLayout, viewPager2,true,false,
+            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                tab.setText(mainPagerAdapter.fragmentItems[position].titleRes)
+                tab.setIcon(mainPagerAdapter.fragmentItems[position].iconRes)
+            }).attach()
     }
 }
